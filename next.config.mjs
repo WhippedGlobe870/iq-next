7c1c4d1fd64bad952fffe3afc1c2a50bd85e3b31
Евгений Бобровничий <viktor.ivanov2b@gmail.com>
2025-11-26 00:20:11 +0300

    123

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   reactCompiler: true,
-  images: {
+   images: {
     domains: ['headless.zbinfo.site'],
   },
-  distDir: '.next',
-}
+};
 
-export default nextConfig
+export default nextConfig;
