cb9533f42f2901f74fb975789e982c8acfd9a7e7
Евгений Бобровничий <viktor.ivanov2b@gmail.com>
2025-11-26 00:29:45 +0300

    1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['headless.zbinfo.site'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'headless.zbinfo.site',
+      },
+    ],
   },
 }
 
